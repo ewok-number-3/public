550a4b61ef083c056ed13cb409118a757a59b036
Brandon Munro-March <53309673+ewok-number-3@users.noreply.github.com>
2021-06-18 13:37:19 +0100

    Create LabStructure.ps1

---
+++ LabStructure.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $SuppliedPass,
+    [Parameter()]
+    [String]
+    $UserNum,
+    [Parameter()]
+    [String]
+    $SAPass    
+)
+#Setup Variables
+$DCRoot = ([ADSI]"").distinguishedName
+$DCUSERDNSDOMAIN = $env:USERDNSDOMAIN
+$WVDRoot = "OU=WVD,$($DCRoot)"
+$password = ConvertTo-SecureString "$($SuppliedPass)" -AsPlainText -Force
+#Create Root WVD OU
+New-ADOrganizationalUnit -Name "WVD" -Path "$($DCRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Environment"
+#Create Other OUs
+New-ADOrganizationalUnit -Name "Users" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Users"
+New-ADOrganizationalUnit -Name "Service Accounts" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Service Accounts"
+New-ADOrganizationalUnit -Name "Servers" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Servers"
+New-ADOrganizationalUnit -Name "Session Hosts" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD WVD Session Hosts"
+#Create Student Accounts within Domain
+for ($User = 1; $User -le $UserNum; $User++)
+{
+    $User
+    New-ADUser -Name "Student$($User) Account" -AccountPassword $password -DisplayName "Student$($User) Account" -Enabled $True -Path "OU=Users,$($WVDRoot)" -SamAccountName "Student$($User)" -Surname "Student$($User)" -UserPrincipalName "Student$($User)@$($DCUSERDNSDOMAIN)"    
+}

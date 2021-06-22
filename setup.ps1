1025d571b0aac4cf42adbdb39260fe2c4b14f8db
Brandon Munro-March <53309673+ewok-number-3@users.noreply.github.com>
2021-06-22 23:59:05 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -7,15 +7,16 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name Az.DesktopVirtualization -RequiredVersion 2.1.0 -force
 Install-Module -Name Az -force
 #Choco install and Choco Apps
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.orginstall.ps1'))
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install googlechrome -y
 choco install putty -y
 choco install notepadplusplus -y
 choco install winscp -y
 choco install sysinternals -y
 choco install bginfo -y
+choco install windows-admin-center -y
 #Setup and Partition Data Disk
-Get-Disk  Where partitionstyle -eq 'raw'  Initialize-Disk -PartitionStyle MBR -PassThru  New-Partition -AssignDriveLetter -UseMaximumSize  Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm$false 
+Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Data" -Confirm "$false"
 #Allow Ping
 Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -enabled True
 Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv6-In)" -enabled True

bf9f478161e8acd70214a75e5e1ffd8eaddcedf3
Brandon Munro-March <53309673+ewok-number-3@users.noreply.github.com>
2021-08-17 17:22:56 +0100

    .

--- wssetup.ps1
+++ wssetup.ps1
@@ -10,11 +10,11 @@ while ($test.PingSucceeded -eq $False)
 Set-ExecutionPolicy Bypass -Scope Process -Force
 #Set PowerShel to use TLS 1.2
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-#Install Az.DesktopVirtualization and configure Start Virtual Machine on resource group and host pool
+<# Install Az.DesktopVirtualization and configure Start Virtual Machine on resource group and host pool
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name Az.DesktopVirtualization -RequiredVersion 2.1.0 -force
 Install-Module -Name Az -force
-#Choco install and Choco Apps
+Choco install and Choco Apps
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install googlechrome -y
 choco install putty -y
@@ -23,7 +23,7 @@ choco install winscp -y
 choco install sysinternals -y
 choco install bginfo -y
 choco install windows-admin-center -y
-#Setup and Partition Data Disk
+#> #Setup and Partition Data Disk
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false
 #Allow Ping
 Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -enabled True

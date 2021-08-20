ede7267941557a1c10fbe48fa8a24bc32af74f1b
Brandon Munro-March <53309673+ewok-number-3@users.noreply.github.com>
2021-08-20 15:54:13 +0100

    update v 1.0

--- ftpsetup.ps1
+++ ftpsetup.ps1
@@ -10,24 +10,22 @@ while ($test.PingSucceeded -eq $False)
 Set-ExecutionPolicy Bypass -Scope Process -Force
 #Set PowerShel to use TLS 1.2
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-<# Install Az.DesktopVirtualization and configure Start Virtual Machine on resource group and host pool
+
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name Az.DesktopVirtualization -RequiredVersion 2.1.0 -force
-Install-Module -Name Az -force
-Choco install and Choco Apps
+
+#Choco install and Choco Apps
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install googlechrome -y
 choco install putty -y
-choco install notepadplusplus -y
 choco install winscp -y
 choco install sysinternals -y
 choco install bginfo -y
-choco install windows-admin-center -y
-#> #Setup and Partition Data Disk
+choco install powershell-core =y
+
+#Setup and Partition Data Disk
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false
 #Allow Ping
 Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -enabled True
-Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv6-In)" -enabled True
-#Install Roles to make Server a Domain Controller
-Install-WindowsFeature ad-domain-services -IncludeManagementTools
-Install-windowsfeature -name DNS -IncludeManagementTools
+#Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv6-In)" -enabled True
+#Install Roles
+Install-WindowsFeature -Name Web-Ftp-Server -IncludeAllSubFeature -IncludeManagementTools

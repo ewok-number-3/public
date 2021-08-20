6400def8f539966d6b56c2ea1a99d5c6affc0ff7
Brandon Munro-March <53309673+ewok-number-3@users.noreply.github.com>
2021-08-20 14:54:28 +0100

    Update Join-domain

--- join-domain.ps1
+++ join-domain.ps1
@@ -15,25 +15,11 @@ param (
     [String]
     $DomainController
 )
-$MSSvrLog = "C:\MSSvrLog.txt"
-
-IF (Test-path $MSSvrLog)
-{Write-host "Log file exits"} 
-else
- { 
- Write-host "Creating log file"
- New-Item $MSSvrLog 
- }
- 
-Add-content $MSSvrLog "Write parameters coming from the script $($DomainPass), $($DomainFQDN), $($DomainUserName), $($DomainController), at the following date/time  $(Get-Date)"
 
 $DomainNB = $DomainFQDN.Split(".")[0]
 $User = "$($DomainNB)\$($DomainUserName)"
 $password = ConvertTo-SecureString "$($DomainPass)" -AsPlainText -Force
 $credental = New-Object System.Management.Automation.PSCredential ($User,$password)
-
-Add-content $MSSvrLog "Write parameters updated with in the script $($DomainNB), $($password), $($credental), $($User),  at the following date/time  $(Get-Date)"
-Add-content $MSSvrLog "Write parameters updated with in the script $($credental.GetNetworkCredential().Domain), $($credental.GetNetworkCredential().UserName), $($credental.GetNetworkCredential().Password),  at the following date/time  $(Get-Date)"
 
 Import-Module ADDSDeployment
 
@@ -44,21 +30,6 @@ do
 }
 while ($test.PingSucceeded -eq $False)
 
-Start-Sleep -Seconds 120
 
 #Add-Computer -DomainName $DomainFQDN -Credential $credental -Restart
 
-Install-ADDSDomainController `
--NoGlobalCatalog:$True `
--CreateDnsDelegation:$false `
--Credential:$credental `
--CriticalReplicationOnly:$false `
--DatabasePath "E:\Data" `
--DomainName:$DomainFQDN `
--InstallDns:$true `
--LogPath "E:\Logs" `
--NoRebootOnCompletion:$false `
--SiteName "Default-First-Site-Name" `
--SysvolPath "E:\SYSVOL" `
--SafeModeAdministratorPassword:$password `
--Force:$true

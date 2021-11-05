11353490ab2ed26c2ea9ad82544d12e57f5b361c
Brandon Munro-March <53309673+ewok-number-3@users.noreply.github.com>
2021-11-05 14:55:41 +0000

    Updates

--- LabStructure.ps1
+++ LabStructure.ps1
@@ -2,29 +2,42 @@
 param (
     [Parameter()]
     [String]
-    $SuppliedPass,
+    $DomainPass,
     [Parameter()]
     [String]
     $UserNum,
     [Parameter()]
     [String]
-    $SAPass    
+    $DomainFQDN   
 )
 #Setup Variables
 $DCRoot = ([ADSI]"").distinguishedName
 $DCUSERDNSDOMAIN = $env:USERDNSDOMAIN
-$WVDRoot = "OU=WVD,$($DCRoot)"
-$password = ConvertTo-SecureString "$($SuppliedPass)" -AsPlainText -Force
+
+$WVDRoot = "OU=UKHO FTP Accounts,$($DCRoot)"
+$WVDRoot = "OU=OU=UKHO FTPS Accounts,$($DCRoot)"
+
+#$WVDRoot = "OU=WVD,$($DCRoot)"
+$password = ConvertTo-SecureString "$($DomainPass)" -AsPlainText -Force
+
 #Create Root WVD OU
-New-ADOrganizationalUnit -Name "WVD" -Path "$($DCRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Environment"
+New-ADOrganizationalUnit -Name "UKHO FTP Accounts" -Path "$($DCRoot)" -ProtectedFromAccidentalDeletion $False -Description "FTP"
 #Create Other OUs
-New-ADOrganizationalUnit -Name "Users" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Users"
-New-ADOrganizationalUnit -Name "Service Accounts" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Service Accounts"
-New-ADOrganizationalUnit -Name "Servers" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Servers"
-New-ADOrganizationalUnit -Name "Session Hosts" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD WVD Session Hosts"
+New-ADOrganizationalUnit -Name "UKHO FTP Users" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTP Users"
+New-ADOrganizationalUnit -Name "UKHO FTP Groups" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTP Groups"
+#New-ADOrganizationalUnit -Name "Servers" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Servers"
+#New-ADOrganizationalUnit -Name "Session Hosts" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD WVD Session Hosts"
+
+#Create Root WVD OU
+New-ADOrganizationalUnit -Name "UKHO FTPS Accounts" -Path "$($DCRoot)" -ProtectedFromAccidentalDeletion $False -Description "FTPS"
+#Create Other OUs
+New-ADOrganizationalUnit -Name "UKHO FTPS Users" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTPS Users"
+New-ADOrganizationalUnit -Name "UKHO FTPS Groups" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTPS Groups"
+#New-ADOrganizationalUnit -Name "Servers" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Servers"
+#New-ADOrganizationalUnit -Name "Session Hosts" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD WVD Session Hosts"
 #Create Student Accounts within Domain
-for ($User = 1; $User -le $UserNum; $User++)
-{
-    $User
-    New-ADUser -Name "Student$($User) Account" -AccountPassword $password -DisplayName "Student$($User) Account" -Enabled $True -Path "OU=Users,$($WVDRoot)" -SamAccountName "Student$($User)" -Surname "Student$($User)" -UserPrincipalName "Student$($User)@$($DCUSERDNSDOMAIN)"    
-}
+#for ($User = 1; $User -le $UserNum; $User++)
+#{
+#    $User
+#    New-ADUser -Name "Student$($User) Account" -AccountPassword $password -DisplayName "Student$($User) Account" -Enabled $True -Path "OU=Users,$($WVDRoot)" -SamAccountName "Student$($User)" -Surname "Student$($User)" -UserPrincipalName "Student$($User)@$($DCUSERDNSDOMAIN)"    
+#}

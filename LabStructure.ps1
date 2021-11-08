543b73d907b4d4fc28c80302d0e50fb113f4b564
Brandon Munro-March <53309673+ewok-number-3@users.noreply.github.com>
2021-11-08 13:15:09 +0000

    Wrong names

--- LabStructure.ps1
+++ LabStructure.ps1
@@ -14,8 +14,8 @@ param (
 $DCRoot = ([ADSI]"").distinguishedName
 $DCUSERDNSDOMAIN = $env:USERDNSDOMAIN
 
-$WVDRoot = "OU=UKHO FTP Accounts,$($DCRoot)"
-$WVDRoot = "OU=OU=UKHO FTPS Accounts,$($DCRoot)"
+$FTPRoot = "OU=UKHO FTP Accounts,$($DCRoot)"
+$FTPSRoot = "OU=OU=UKHO FTPS Accounts,$($DCRoot)"
 
 #$WVDRoot = "OU=WVD,$($DCRoot)"
 $password = ConvertTo-SecureString "$($DomainPass)" -AsPlainText -Force
@@ -23,16 +23,16 @@ $password = ConvertTo-SecureString "$($DomainPass)" -AsPlainText -Force
 #Create Root WVD OU
 New-ADOrganizationalUnit -Name "UKHO FTP Accounts" -Path "$($DCRoot)" -ProtectedFromAccidentalDeletion $False -Description "FTP"
 #Create Other OUs
-New-ADOrganizationalUnit -Name "UKHO FTP Users" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTP Users"
-New-ADOrganizationalUnit -Name "UKHO FTP Groups" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTP Groups"
+New-ADOrganizationalUnit -Name "UKHO FTP Users" -Path "$($FTPRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTP Users"
+New-ADOrganizationalUnit -Name "UKHO FTP Groups" -Path "$($FTPRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTP Groups"
 #New-ADOrganizationalUnit -Name "Servers" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Servers"
 #New-ADOrganizationalUnit -Name "Session Hosts" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD WVD Session Hosts"
 
 #Create Root WVD OU
 New-ADOrganizationalUnit -Name "UKHO FTPS Accounts" -Path "$($DCRoot)" -ProtectedFromAccidentalDeletion $False -Description "FTPS"
 #Create Other OUs
-New-ADOrganizationalUnit -Name "UKHO FTPS Users" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTPS Users"
-New-ADOrganizationalUnit -Name "UKHO FTPS Groups" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTPS Groups"
+New-ADOrganizationalUnit -Name "UKHO FTPS Users" -Path "$($FTPSRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTPS Users"
+New-ADOrganizationalUnit -Name "UKHO FTPS Groups" -Path "$($FTPSRoot)" -ProtectedFromAccidentalDeletion $False -Description "UKHO FTPS Groups"
 #New-ADOrganizationalUnit -Name "Servers" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD Servers"
 #New-ADOrganizationalUnit -Name "Session Hosts" -Path "$($WVDRoot)" -ProtectedFromAccidentalDeletion $False -Description "WVD WVD Session Hosts"
 #Create Student Accounts within Domain
